d6032d83eaee16c892ad7fe2c10d4a70c1fbf28d
匡光淼 <guangmiao.kuang@qizhidao.com>
2022-08-26 17:40:18 +0800

    feat: ##

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -4,7 +4,7 @@
  * @Autor: 匡光淼
  * @Date: 2022-08-26 09:45:34
  * @LastEditors: 匡光淼
- * @LastEditTime: 2022-08-26 15:40:25
+ * @LastEditTime: 2022-08-26 17:34:05
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -19,15 +19,19 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
       name: 'NiceVue',
-      fileName: 'nice-vue'
+      fileName: 'nicevue'
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
       external: ['vue'],
       output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: { vue: 'Vue' }
+        globals: { vue: 'Vue' } // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
       }
+      // output: [
+      //   // 配置了这个output，上面配置的lib就没用了
+      //   { format: 'es', dir: './dist/es', entryFileNames: '[name].js', preserveModules: true, preserveModulesRoot: 'src' },
+      //   { format: 'umd', dir: './dist', entryFileNames: 'index.umd.cjs', name: 'NiceVue' }
+      // ]
     }
   }
 })
