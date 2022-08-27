86cd816fb099aa58d9d168a0400e6e2de77e959d
匡光淼 <guangmiao.kuang@qizhidao.com>
2022-08-27 10:21:09 +0800

    feat: 完成全局样式导入

--- packages/site/src/main.ts
+++ packages/site/src/main.ts
@@ -1,7 +1,14 @@
+/*
+ * @Description:
+ * @Version: 2.0
+ * @Autor: 匡光淼
+ * @Date: 2022-08-26 09:46:46
+ * @LastEditors: 匡光淼
+ * @LastEditTime: 2022-08-27 10:03:32
+ */
 import { createApp } from 'vue'
 import App from './App'
 import NiceVue from '@nicevue/ui'
-// import '@nicevue/ui/dist/style/index.css'
-import '../node_modules/@nicevue/ui/dist/style/index.css'
+import '@nicevue/ui/dist/style/index.css'
 
 createApp(App).use(NiceVue).mount('#app')
